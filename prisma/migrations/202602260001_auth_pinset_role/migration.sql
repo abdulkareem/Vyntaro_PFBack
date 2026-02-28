ddef2b24d951d5e4273b1e0a7eb2745f6985729c
abdulkareem <abdulkareem.t@gmail.com>
2026-02-28 22:32:01 +0530

    Delete prisma/migrations directory

--- prisma/migrations/202602260001_auth_pinset_role/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN', 'SUPER_ADMIN');
-
--- AlterTable
-ALTER TABLE "UserAccount"
-ADD COLUMN     "pinSet" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'USER';
